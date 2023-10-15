3dd65d2c5389d068792a846e654999304e02534d
vickykanitham <147973173+vickykanitham@users.noreply.github.com>
2023-10-15 14:17:02 +0530

    Add files via upload
    FindNumber

---
+++ findNumber.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class findNumber {
+	public static void main(String[] args) {
+		int number=45;
+		if(number>0) {
+			System.out.println("The number is positive");
+		}
+		else
+		{
+			System.out.println("The number is Negative");
+		}
+	}
+
+}
