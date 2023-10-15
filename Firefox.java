0e1b25a8041407f4c2c3d25b5ba3370b48993c46
vickykanitham <147973173+vickykanitham@users.noreply.github.com>
2023-10-15 14:02:37 +0530

    Add files via upload
    Day1

---
+++ Firefox.java
@@ -0,0 +1,13 @@
+package adcb;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean pageLoaded=true;
+		int noOfBrowser=1;
+		char browserLogo='f';
+		System.out.println(browserVersion+" "+browserName+" " +pageLoaded+" " +noOfBrowser+" " +browserLogo);
+	}
+
+}
